d6b2e7bde3ad508485dcca0efe7f7a6425cead12
Md Maruf Howlader <maruf.dhaka2010@gmail.com>
2020-09-16 14:15:27 +0600

    Core data to list Create and Read

--- CoreData_Project2/AppDelegate.swift
+++ CoreData_Project2/AppDelegate.swift
@@ -15,7 +15,8 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
 
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        // Override point for customization after application launch.
+        let urlDirectory = FileManager.default.urls(for: .documentDirectory, in: .userDomainMask)
+        print(urlDirectory[urlDirectory.count-1] as URL)
         return true
     }
 
